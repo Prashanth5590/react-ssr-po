8f3375c8bc8675a00ed7eb20538f905c407f3e6d
Prashanth5590 <83853883+Prashanth5590@users.noreply.github.com>
2025-09-03 21:08:09 +0530

    Update page.tsx

--- src/app/games/page.tsx
+++ src/app/games/page.tsx
@@ -1,31 +1,35 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-export const dynamic = "force-dynamic"; // always fetch fresh data
+import  { GamesWidget } from "@ps_devops/test-ssr-widget";
+export const dynamic = "force-dynamic";
 
-async function getGames(  ) {
-
+async function getGames() {
   const res = await fetch(
     "https://qa-pokerstars-de-api.uatsecure.com/ajax/game/getGames",
     { cache: "no-store" }
   );
 
-  if (!res.ok) {
-    throw new Error("Failed to fetch games");
-  }
+  if (!res.ok) throw new Error("Failed to fetch games");
 
   return res.json();
 }
 
 export default async function GamesPage() {
   const games = await getGames();
+return (
+    <main style={{ fontFamily: "sans-serif", padding: "2rem" }}>
+      <p>
+        Hi Prashanth, welcome to the very first <strong>SSR-powered
+        application</strong>!<br />
+        It’s great to see everything running smoothly —
+        from server-side rendering to reusable React components.
+      </p>
+      <p>
+        This is just the beginning — imagine how much fun and how many
+        features we can build on top of this foundation.
+      </p>
 
-  return (
-    <main>
-      <h1>Games List</h1>
-      <ul>
-        {games.map((g: any, i: number) => (
-          <li key={i}>{g.name}</li>
-        ))}
-      </ul>
+    <h1>Games List (SSR) - POKERSTARS - DE - QA Env</h1>
+  
+      <GamesWidget games={games} />
     </main>
   );
 }
